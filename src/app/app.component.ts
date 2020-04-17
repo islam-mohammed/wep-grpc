0fbeef52e19bc361cfe1ba0ddfe36cd330adce1f
islam-mohammed <islammohammed1978@gmail.com>
2020-04-17 18:24:44 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as grpcWeb from "grpc-web"
+import { AssetServiceClient, ListAssetsRequest, ListAssetsResponse } from 'pnp-sdk';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'gRPC';
+
+  ngOnInit() {
+    const assetsService = new AssetServiceClient(environment.pnp_grpc_api);
+    const request = new ListAssetsRequest();
+    request.setName("APP_ASSETS");
+    const meta = {
+       'accept-language': 'en',
+       'x-pnp-client-platform': 'web_app_desktop'
+    }
+    assetsService.listAssets(request, meta, (err, response: ListAssetsResponse) => {
+      if (err) {
+        console.log(err.code);
+        console.log(err.message);
+      } else {
+        console.log(response.toObject().assetsMap);
+      }
+    });
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  pnp_grpc_api: 'http://beta1.portablenorthpole.com:80'
 };
 
 /*
